e164ab938f87488481c5497655d605d14d7af8ef
Sway <godswaydk75@gmail.com>
2025-11-10 09:55:51 +0000

    2-arguments.js

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,6 @@
 let lang = 'C is fun';
 let lang1 = 'Python is cool';
-let lang2 = 'JavaScript is amazing'
-console.log(lang);
-console.log(lang1);
-console.log(lang2);
+let lang2 = 'JavaScript is amazing';
+console.log(`${lang}, 
+${lang1},
+${lang2}`);
